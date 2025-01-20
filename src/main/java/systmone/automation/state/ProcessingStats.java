032dad0ea05ddf23859c1a538fd886a5460bf272
AWhite-Code <Alexwhitehead432@gmail.com>
2025-01-20 14:35:53 +0000

    Updated ProcessingStats to follow java encapsulation best practices

--- src/main/java/systmone/automation/state/ProcessingStats.java
+++ src/main/java/systmone/automation/state/ProcessingStats.java
@@ -6,11 +6,35 @@ import java.util.List;
 import systmone.automation.document.DocumentError;
 
 public class ProcessingStats {
-    int totalDocuments;
-    int processedDocuments;
-    List<DocumentError> errors;
+    private int totalDocuments;
+    private int processedDocuments;
+    private List<DocumentError> errors;
     
-    ProcessingStats() {
+    public ProcessingStats() {
         this.errors = new ArrayList<>();
+    }
+    
+    public int getTotalDocuments() {
+        return totalDocuments;
+    }
+    
+    public void setTotalDocuments(int totalDocuments) {
+        this.totalDocuments = totalDocuments;
+    }
+    
+    public int getProcessedDocuments() {
+        return processedDocuments;
+    }
+    
+    public void setProcessedDocuments(int processedDocuments) {
+        this.processedDocuments = processedDocuments;
+    }
+    
+    public List<DocumentError> getErrors() {
+        return errors;
+    }
+    
+    public void addError(DocumentError error) {
+        this.errors.add(error);
     }
 }
