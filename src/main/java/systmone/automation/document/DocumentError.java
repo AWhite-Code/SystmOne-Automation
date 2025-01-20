913cdf43754233cd7600b8f42bbc986f0511fe57
AWhite-Code <Alexwhitehead432@gmail.com>
2025-01-20 14:55:14 +0000

    Updated Document Error to show its public functions.

--- src/main/java/systmone/automation/document/DocumentError.java
+++ src/main/java/systmone/automation/document/DocumentError.java
@@ -1,11 +1,22 @@
 package systmone.automation.document;
 
 public class DocumentError {
-    final int documentIndex;
-    final String errorMessage;
+    // These fields are private but final to maintain immutability
+    private final int documentIndex;
+    private final String errorMessage;
     
-    DocumentError(int index, String message) {
+    // Public constructor since we need to create these from other packages
+    public DocumentError(int index, String message) {
         this.documentIndex = index;
         this.errorMessage = message;
+    }
+    
+    // Getter methods to access the private fields
+    public int getDocumentIndex() {
+        return documentIndex;
+    }
+    
+    public String getErrorMessage() {
+        return errorMessage;
     }
 }
